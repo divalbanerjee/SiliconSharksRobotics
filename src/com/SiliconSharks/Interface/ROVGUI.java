12eeff425b75b62e092a290707671baea02e78a4
Dival Banerjee <bandiv2011@gmail.com>
2017-09-03 14:17:11 -0400

    Push should be fixed﻿

--- src/com/SiliconSharks/Interface/ROVGUI.java
+++ src/com/SiliconSharks/Interface/ROVGUI.java
@@ -1,7 +1,97 @@
 package com.SiliconSharks.Interface;
 
+import com.SiliconSharks.Graphics.Compass;
+import com.SiliconSharks.Graphics.ControllerInterface;
+import com.SiliconSharks.Graphics.MButton;
+import com.SiliconSharks.Graphics.StatusBar;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by bandi on 8/27/2017.
  */
-public class ROVGUI {
+public class ROVGUI extends JFrame {
+    private Font font20Pt = new Font("Helvetica" , Font.PLAIN, 20);
+    Container container;
+    Color textColor = new Color(149, 165, 166);
+    ControllerInterface controller1 = new ControllerInterface(1);
+    ControllerInterface controlller2 = new ControllerInterface(2);
+    Compass xAxis = new Compass("X Axis Rotation");
+    Compass yAxis  = new Compass("Y Axis Rotation");
+    Compass zAxis = new Compass("Z Axis Rotation");
+    MButton btnShutDown = new MButton("Shut Down");
+    StatusBar rovStatusBar = new StatusBar();
+
+    public ROVGUI(){
+
+        JPanel layoutManager = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        btnShutDown.setBackColor(new Color(229, 57, 53));
+        btnShutDown.setPaddingColor(new Color(183, 28, 28));
+        btnShutDown.setPadding(5);
+        btnShutDown.setBackGroundColor(new Color(84, 110, 122));
+        setSize(1900,1200);
+        setBackground(new Color(44, 62, 80));
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
+        setVisible(true);
+        container = getContentPane();
+        container.setBackground(new Color(44,62,80));
+        layoutManager.setBackground(new Color(44,62,80));
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx  = .7;
+
+        c.weighty = .2;
+
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.fill = GridBagConstraints.BOTH;
+
+        layoutManager.add(rovStatusBar,c);
+
+        c.weighty = .4;
+        c.gridy = 1;
+        c.weightx = 1;
+        layoutManager.add(controller1,c);
+        c.gridx = 1;
+        layoutManager.add(controlller2,c);
+/*
+        c.gridx = 2;
+        c.gridy = 0;
+        c.weightx = .25;
+        c.weighty = .1;
+*/
+       // JPanel dataPanel = new JPanel(new GridLayout(6,1));
+       // dataPanel.setBackground(new Color(84, 110, 122));
+
+       // dataPanel.add(xAxis);
+       // dataPanel.add(yAxis);
+       // dataPanel.add(zAxis);
+
+        c.gridx = 2;
+        c.gridy = 0;
+        c.weightx = .3;
+        c.weighty = .2;
+
+        c.ipady = 10;
+
+        layoutManager.add(xAxis,c);
+
+        c.gridy = 1;
+
+        layoutManager.add(yAxis,c);
+
+        c.gridy = 2;
+
+        layoutManager.add(zAxis,c);
+
+        c.gridy = 3;
+        layoutManager.add(btnShutDown,c);
+
+        //layoutManager.add(dataPanel, c);
+        container.add(layoutManager);
+    }
 }
--- src/com/SiliconSharks/Interface/mainGUI.java
+++ src/com/SiliconSharks/Interface/mainGUI.java
@@ -1,7 +1,12 @@
 package com.SiliconSharks.Interface;
 
+import javax.swing.*;
+
 /**
  * Created by bandi on 8/27/2017.
  */
 public class mainGUI {
+    public static void main(String[]args){
+        JFrame rovGUI = new ROVGUI();
+    }
 }
